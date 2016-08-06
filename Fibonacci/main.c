d0869eda60339511224b2df2d2ebe05579b3d8f2
陈宇峰 <yufengcode@gmail.com>
2016-08-06 14:20:49 +0800

    C实现斐波那契数列

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -6,10 +6,32 @@
 //  Copyright © 2016 chenyufeng. All rights reserved.
 //
 
+// C语言实现斐波那契数列
+// 0 1 1 2 3 5 8 13 21
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int Fibonacci(int num);
+
+int main(int argc, const char * argv[])
+{
+    int index = Fibonacci(6);
+    printf("%d ",index);
     return 0;
+}
+
+int Fibonacci(int num)
+{
+    // 递归结束条件
+    if (num == 0)
+    {
+        return 0;
+    }
+
+    if (num == 1)
+    {
+        return 1;
+    }
+
+    return Fibonacci(num - 1) + Fibonacci(num - 2);
 }
